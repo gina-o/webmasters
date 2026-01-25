991e4bb659e5e398d933574c78d14673eceb2528
Temilola Adepoju <temiadepoju384@gmail.com>
2026-01-25 17:56:23 -0600

    indented, changed css values

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -48,18 +48,27 @@ export default function Menu() {
         {/* 🌟 HERO */}
     <div class="content">
         {/* 🌀 HOW TO EXPLORE */}
-        <section ref={exploreRef} className="max-w-5xl w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-          {/* Card 1 */}
-          <div className="bg-white/10 backdrop-blur-md p-10 rounded-xl border border-white/20">
-            <h2 className="text-3xl font-bold mb-4">Explore Neighborhoods</h2>
-            <p>Click through Houston’s communities...</p>
-            <button
-  onClick={() => navigate("/map")}
-  className="mt-4 bg-cyan-500 hover:bg-cyan-700 text-white py-2 px-4 rounded-lg transition duration-200 shadow-md"
->
-  Check out map
-</button>
-          </div>
+        <section
+                ref={exploreRef}
+                className="relative min-h-screen content z-20"
+              >
+
+                <div className="flex justify-center px-6 lg:px-32 py-40">
+
+                  <div className="max-w-5xl w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+                    {/* Card 1 */}
+                    <div className="bg-white/10 backdrop-blur-md p-10 rounded-xl border border-white/20">
+                      <h2 className="text-3xl font-bold mb-4">
+                        Explore Neighborhoods
+                      </h2>
+                      <p>Click through Houston’s communities...</p>
+                      <button
+                        onClick={() => navigate("/map")}
+                        className="mt-4 bg-cyan-500 hover:bg-cyan-700 py-2 px-4 rounded-lg"
+                      >
+                        Check out map
+                      </button>
+                    </div>
 
           {/* Card 2 */}
           <div className="bg-white/10 backdrop-blur-md p-10 rounded-xl border border-white/20">
