9794bca7777f781d148f5b6b8b6ea89149bef892
gina-o <gina.ovanessians@gmail.com>
2026-01-26 14:28:33 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(), // React plugin
-  ],
-});
+  plugins: [react()],
+  base: '/webmasters/' // THIS MUST MATCH your GitHub repo name
+})
 
 
