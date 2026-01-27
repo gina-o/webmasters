0265e85f88911754edb6dddea3cf4e817a748116
Temilola Adepoju <temiadepoju384@gmail.com>
2026-01-26 19:36:27 -0600

    changed to hash

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
